0954c956e13ec00ef8d4d20df09f387a95c830c5
lucasedferreira <lucasedbatista@gmail.com>
2022-05-20 18:49:29 -0300

    visual fix in top 10 list

--- frontend/src/components/Top10Card/List.vue
+++ frontend/src/components/Top10Card/List.vue
@@ -1,4 +1,6 @@
 <script setup>
+import ExternalLinkIcon from "@/components/icons/ExternalLink.vue";
+
 defineProps({
   items: {
     type: Array,
@@ -12,19 +14,20 @@ defineProps({
     <h2 class="list__title">Top Songs</h2>
     <div class="list__item" v-for="(item, index) in items" :key="item.id">
       <div class="list__item--rank">{{ ++index }}</div>
-      <div class="list__item--name">{{ `${item.name} - ${item.artist}` }}</div>
+      <div class="list__item--name">{{ item.name + " - " + item.artist }}</div>
+      <!-- <div class="list__item--link"><ExternalLinkIcon /></div> -->
     </div>
   </div>
 </template>
 
 <style scoped lang="scss">
 .list {
-  margin: 5rem 2rem;
+  margin: 2rem;
   font-family: "Montserrat", sans-serif;
   font-weight: 300;
 
   &__title {
-    font-size: 4rem;
+    font-size: 3.3rem;
     text-align: center;
     color: #ec1e1e;
 
@@ -35,27 +38,36 @@ defineProps({
 
   &__item {
     color: #f7d1dc;
-    font-size: 2.4rem;
-
+    font-size: 2rem;
     width: 100%;
+    margin-bottom: 0.8rem;
+    line-height: 3rem;
+
     display: grid;
-    grid-template-columns: 3.5rem auto;
+    grid-template-columns: 3.5rem auto auto;
     grid-gap: 2rem;
 
-    &--rank {
-      grid-column: 1 / 2;
-
-      &:before {
-        content: "#";
-      }
+    &--rank:before {
+      content: "#";
     }
 
     &--name {
-      grid-column: 2 / -1;
+      // text-align: justify;
+    }
+
+    &--link {
+      text-align: end;
+      justify-self: end;
+
+      * {
+        width: 3rem;
+        height: 3rem;
+        fill: #f7d1dc;
+      }
     }
 
     @media only screen and (max-width: $bp-small) {
-      font-size: 3rem;
+      font-size: 2.3rem;
     }
   }
 }
