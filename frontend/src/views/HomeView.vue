e366434be534de1db4fb16811f46455e19606176
lucasedferreira <lucasedbatista@gmail.com>
2022-05-21 12:28:22 -0300

    finish home page

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -39,28 +39,103 @@ const link = generateSpotifyLink();
 
 <template>
   <div class="home">
-    <div class="title">Start seeing what you listen</div>
-    <div class="login">
-      <img class="circle" src="/circle.png" alt="Hand Drown Circle" />
-      <a class="login-button" :href="link">
-        <img
-          class="login-button__image"
-          src="/spotify-logo-white.png"
-          alt="Spotify Logo"
-        />
-        <span class="login-button__text">Login with Spotify</span>
-      </a>
+    <div class="home__content">
+      <div class="title">Start <strong>seeing</strong> what you <strong>listen</strong></div>
+      <div class="login">
+        <img class="circle" src="/circle.png" alt="Hand Drown Circle" />
+        <a class="login-button" :href="link">
+          <img
+            class="login-button__image"
+            src="/spotify-logo-white.png"
+            alt="Spotify Logo"
+          />
+          <span class="login-button__text">Login with Spotify</span>
+        </a>
+      </div>
     </div>
+    <img src="/note-1.png" alt="Note 1" class="home__images home__images--1" />
+    <img src="/note-2.png" alt="Note 2" class="home__images home__images--2" />
+    <img src="/earphones.png" alt="Ear Phones" class="home__images home__images--3" />
   </div>
 </template>
 
 <style lang="scss">
 .home {
   height: 100vh;
+  display: grid;
+  grid-template-columns: repeat(10, 10vw);
+  grid-template-rows: repeat(10, 10vh);
+
+  &__content {
+    grid-column: 3 / 9;
+    grid-row: 2 / -1;
+
+    @media only screen and (max-width: $bp-medium) {
+      grid-column: 2 / 10;
+    }
+
+    @media only screen and (max-width: $bp-small) {
+      grid-column: 1 / -1;
+    }
+  }
+
+  &__images {
+    &--1 {
+      width: 5rem;
+      grid-column: 9;
+      grid-row: 2;
+      justify-self: end;
+
+      @media only screen and (max-width: $bp-medium) {
+        grid-column: 10;
+        justify-self: center;
+      }
+
+      @media only screen and (max-width: $bp-small) {
+        width: 3rem;
+        grid-column: 9;
+        grid-row: 4;
+        justify-self: end;
+        align-self: start;
+      }
+    }
+
+    &--2 {
+      width: 5rem;
+      grid-column: 3;
+      grid-row: 5;
+
+      @media only screen and (max-width: $bp-medium) {
+        grid-column: 2;
+        grid-row: 5;
+      }
+
+      @media only screen and (max-width: $bp-small) {
+        width: 4rem;
+        grid-column: 2;
+        grid-row: 4;
+        justify-self: center;
+        align-self: end;
+      }
+    }
+
+    &--3 {
+      height: 35rem;
+      grid-column: 7;
+      grid-row: 7 / -1;
+      align-self: end;
+
+      @media only screen and (max-width: $bp-medium) {
+        height: 30rem;
+        grid-column: 1 / -1;
+        justify-self: end;
+      }
+    }
+  }
 }
 
 .title {
-  padding-top: 10vh;
+  // padding-top: 10vh;
   font-size: 10rem;
   color: $color-white;
   text-align: center;
@@ -80,11 +155,7 @@ const link = generateSpotifyLink();
 }
 
 .circle {
-  width: 35vw;
-
-  @media only screen and (max-width: $bp-small) {
-    width: 80vw;
-  }
+  width: 35rem;
 }
 
 .login-button {
@@ -113,6 +184,10 @@ const link = generateSpotifyLink();
     color: $color-primary;
     font-size: 2rem;
     font-weight: bold;
+  }
+
+  &:hover {
+    background-color: #20C75A;
   }
 }
 </style>
